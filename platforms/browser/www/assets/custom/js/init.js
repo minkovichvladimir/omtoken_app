ec254ceb1df0d6c1d63b3bf86c221c424c165f13
Minkovich Vladimir <minkovichvladimir@gmail.com>
2018-04-20 00:47:23 +0300

    set light indigo theme

--- platforms/browser/www/assets/custom/js/init.js
+++ platforms/browser/www/assets/custom/js/init.js
@@ -1,39 +1,39 @@
 'use strict';
 
-(function() {
+(function () {
 
-/*
-|------------------------------------------------------------------------------
-| Initialize Framework7
-| For more parameters visit https://framework7.io/docs/init-app.html
-|------------------------------------------------------------------------------
-*/
+    /*
+    |------------------------------------------------------------------------------
+    | Initialize Framework7
+    | For more parameters visit https://framework7.io/docs/init-app.html
+    |------------------------------------------------------------------------------
+    */
 
-window.myApp = new Framework7({
-	cache: false,
-	init: false,
-	material: true,
-	modalTitle: 'OmToken',
-	notificationCloseButtonText: 'OK',
-	scrollTopOnNavbarClick: true
-});
+    window.myApp = new Framework7({
+        cache: false,
+        init: false,
+        material: true,
+        modalTitle: 'Open Market Token',
+        notificationCloseButtonText: 'OK',
+        scrollTopOnNavbarClick: true
+    });
 
-/*
-|------------------------------------------------------------------------------
-| Initialize Main View
-|------------------------------------------------------------------------------
-*/
+    /*
+    |------------------------------------------------------------------------------
+    | Initialize Main View
+    |------------------------------------------------------------------------------
+    */
 
-window.mainView = myApp.addView('.view-main');
+    window.mainView = myApp.addView('.view-main');
 
-/*
-|------------------------------------------------------------------------------
-| Assign Dom7 Global Function to a variable $$ to prevent conflicts with other
-| libraries like jQuery or Zepto.
-|------------------------------------------------------------------------------
-*/
+    /*
+    |------------------------------------------------------------------------------
+    | Assign Dom7 Global Function to a variable $$ to prevent conflicts with other
+    | libraries like jQuery or Zepto.
+    |------------------------------------------------------------------------------
+    */
 
-window.$$ = Dom7;
+    window.$$ = Dom7;
 
 })();
 
@@ -44,11 +44,11 @@ window.$$ = Dom7;
 */
 
 $$(document).on('ajaxStart', function (e) {
-	myApp.showIndicator();
+    myApp.showIndicator();
 });
 
 $$(document).on('ajaxComplete', function (e) {
-	myApp.hideIndicator();
+    myApp.hideIndicator();
 });
 
 /*
@@ -57,23 +57,26 @@ $$(document).on('ajaxComplete', function (e) {
 |------------------------------------------------------------------------------
 */
 
-$$(document).on('pageInit', function(e) {
-	if (sessionStorage.getItem('nectarMaterialThemeColor')) {
-		$$('body').removeClass('theme-red theme-pink theme-purple theme-deeppurple theme-indigo theme-blue theme-lightblue theme-cyan theme-teal theme-green theme-lightgreen theme-lime theme-yellow theme-amber theme-orange theme-deeporange theme-brown theme-gray theme-bluegray theme-white theme-black');
-		$$('body').addClass('theme-' + sessionStorage.getItem('nectarMaterialThemeColor'));
-	}
-	
-	if (sessionStorage.getItem('nectarMaterialThemeLayout')) {
-		switch(sessionStorage.getItem('nectarMaterialThemeLayout')) {
-			case 'dark':
-				$$('body').removeClass('layout-dark');
-				$$('body').addClass('layout-' + sessionStorage.getItem('nectarMaterialThemeLayout'));
-			break;
-			default:
-				$$('body').removeClass('layout-dark');
-			break;
-		}
-	}
+$$(document).on('pageInit', function (e) {
+    if (sessionStorage.getItem('nectarMaterialThemeColor')) {
+        $$('body').removeClass('theme-red theme-pink theme-purple theme-deeppurple theme-indigo theme-blue theme-lightblue theme-cyan theme-teal theme-green theme-lightgreen theme-lime theme-yellow theme-amber theme-orange theme-deeporange theme-brown theme-gray theme-bluegray theme-white theme-black');
+        $$('body').addClass('theme-' + sessionStorage.getItem('nectarMaterialThemeColor'));
+    }
+
+    if (sessionStorage.getItem('nectarMaterialThemeLayout')) {
+        switch (sessionStorage.getItem('nectarMaterialThemeLayout')) {
+            case 'dark':
+                $$('body').removeClass('layout-dark');
+                $$('body').addClass('layout-' + sessionStorage.getItem('nectarMaterialThemeLayout'));
+                break;
+            default:
+                $$('body').removeClass('layout-dark');
+                break;
+        }
+    }
 });
+
+sessionStorage.setItem('nectarMaterialThemeLayout', 'light');
+sessionStorage.setItem('nectarMaterialThemeColor', 'indigo');
 
 localStorage.setItem('api_host', 'http://185.51.246.70');
